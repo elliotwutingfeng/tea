1897f14a5c886ff614ed294585ed60a6b6be21a6
Daniel Siepmann <coding@daniel-siepmann.de>
2025-02-03 12:27:27 +0100

    [!!!][TASK] Drop support for PHP < 8.1 (#1576)
    As this is not supported by current TYPO3 LTS versions.
    
    Resolves: #1566

--- Build/Scripts/runTests.sh
+++ Build/Scripts/runTests.sh
@@ -229,10 +229,8 @@ Options:
             - 11.5: use TYPO3 v11 with typo3/cms-composer-installers ^3
             - 12.4: (default) use TYPO3 v12 with typo3/cms-composer-installers ^5
 
-    -p <7.4|8.0|8.1|8.2|8.3|8.4>
+    -p <8.1|8.2|8.3|8.4>
         Specifies the PHP minor version to be used
-            - 7.4: use PHP 7.4
-            - 8.0: use PHP 8.0
             - 8.1: use PHP 8.1
             - 8.2: use PHP 8.2
             - 8.3: (default) use PHP 8.3
@@ -367,7 +365,7 @@ while getopts "a:b:s:d:i:p:e:t:xy:o:nhu" OPT; do
             ;;
         p)
             PHP_VERSION=${OPTARG}
-            if ! [[ ${PHP_VERSION} =~ ^(7.4|8.0|8.1|8.2|8.3|8.4)$ ]]; then
+            if ! [[ ${PHP_VERSION} =~ ^(8.1|8.2|8.3|8.4)$ ]]; then
                 INVALID_OPTIONS+=("-p ${OPTARG}")
             fi
             ;;
