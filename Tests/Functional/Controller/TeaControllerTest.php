ca8105fc19828c9a87f690295a9e2d16ff616c5a
Daniel Siepmann <daniel.siepmann@codappix.com>
2025-04-07 13:27:49 +0200

    [TASK] Cover empty tea index with functional test (#1642)
    Relates: #1565

--- Tests/Functional/Controller/TeaControllerTest.php
+++ Tests/Functional/Controller/TeaControllerTest.php
@@ -46,7 +46,30 @@ final class TeaControllerTest extends FunctionalTestCase
             ],
         ]);
         $this->importCSVDataSet(__DIR__ . '/Fixtures/Database/ContentElementTeaIndex.csv');
-        $this->importCSVDataSet(__DIR__ . '/Fixtures/Database/Teas.csv');
+    }
+
+    /**
+     * @test
+     */
+    public function indexActionShowsMessageWhenNoTeasAreAvailable(): void
+    {
+        $request = (new InternalRequest())->withPageId(1);
+
+        $html = (string)$this->executeFrontendSubRequest($request)->getBody();
+
+        self::assertStringContainsString('No teas available.', $html);
+    }
+
+    /**
+     * @test
+     */
+    public function indexActionShowsNoTableMarkupWhenNoTeasAreAvailable(): void
+    {
+        $request = (new InternalRequest())->withPageId(1);
+
+        $html = (string)$this->executeFrontendSubRequest($request)->getBody();
+
+        self::assertStringNotContainsString('table', $html);
     }
 
     /**
@@ -54,6 +77,8 @@ final class TeaControllerTest extends FunctionalTestCase
      */
     public function indexActionRendersAllAvailableTeas(): void
     {
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/Database/Teas.csv');
+
         $request = (new InternalRequest())->withPageId(1);
 
         $html = (string)$this->executeFrontendSubRequest($request)->getBody();
@@ -67,6 +92,8 @@ final class TeaControllerTest extends FunctionalTestCase
      */
     public function showActionRendersTheGivenTeas(): void
     {
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/Database/Teas.csv');
+
         $request = (new InternalRequest())->withPageId(3)->withQueryParameters(['tx_tea_teashow[tea]' => 1]);
 
         $html = (string)$this->executeFrontendSubRequest($request)->getBody();
